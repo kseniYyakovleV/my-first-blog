b4fb42ade7a16c32bfd75ccebf51c129eb0cf7b3
Your Name <you@example.com>
2022-09-23 21:31:16 +0300

    Ya ustal

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,9 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/staticfile/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
-STATICFILES_DIRS = (
-  '/static/',
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
